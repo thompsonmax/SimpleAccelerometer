88ab685a9954ec29c5009391353fbd56e0abefc1
unknown <thompm22@uw.edu>
2017-07-29 01:42:47 -0700

    initial commit

---
+++ app/src/main/java/com/example/max/simpleaccelerometer/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.max.simpleaccelerometer;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private float lastX, lastY, lastZ;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccel;
+
+    private float deltaX = 0;
+    private float deltaY = 0;
+    private float deltaZ = 0;
+
+    private TextView currentX, currentY, currentZ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initializeViews();
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            // Success: we have an accelerometer
+            mAccel = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    public void initializeViews() {
+        currentX = (TextView) findViewById(R.id.currentX);
+        currentY = (TextView) findViewById(R.id.currentY);
+        currentZ = (TextView) findViewById(R.id.currentZ);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // clean current values
+        displayCleanValues();
+        // display the current x,y,z accelerometer values
+        displayCurrentValues();
+
+        // get the change of the x,y,z values of the accelerometer
+        deltaX = Math.abs(lastX - event.values[0]);
+        deltaY = Math.abs(lastY - event.values[1]);
+        deltaZ = Math.abs(lastZ - event.values[2]);
+
+        // if the change is below 2, it is just plain noise
+
+        if (deltaX < 2)
+            deltaX = 0;
+        if (deltaY < 2)
+            deltaY = 0;
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void displayCleanValues() {
+        currentX.setText("0.0");
+        currentY.setText("0.0");
+        currentZ.setText("0.0");
+    }
+
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, mAccel, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
